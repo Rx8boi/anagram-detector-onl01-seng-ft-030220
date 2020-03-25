a4d17c65929dc29b84d264743f07c21d26a7d46e
Charles Butler <cdbutle@outlook.com>
2020-03-25 01:48:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  
+  anagram_array = []
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @anagram_array = word
+  end
+  
+  def self.match()
+    if anagram_array.select do |word| 
+      word.split(" ") == 
+  else
+    return []
+end
+  
+  
+  
+  
+end
