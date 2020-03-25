484db2ff5002ab2799eff62ea7a1752e6536e16d
Charles Butler <cdbutle@outlook.com>
2020-03-25 01:49:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,10 +9,8 @@ class Anagram
   end
   
   def self.match()
-    if anagram_array.select do |word| 
+    anagram_array.select do |word| 
       word.split(" ") == @anagram_array.split(" ").sort
-  else
-    return []
 end
   
   
