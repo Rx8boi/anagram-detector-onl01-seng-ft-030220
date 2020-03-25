45090575c38c547eee6cb8fdc6748e1a3ce529ce
Charles Butler <cdbutle@outlook.com>
2020-03-25 02:00:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,17 +1,19 @@
 class Anagram
-
-
+  
+  
   attr_accessor :word
-
+  
   def initialize(word)
-    @words = word
+    @word = word
   end
-
-  def match(word_array)
-    word_array.select do |word|
-      word.split("").sort == @words.split("").sort
+  
+  def match(words_array)
+    words_array.select do |word| 
+    word.split(" ").sort == @word.split(" ").sort
     end
   end
+  
+  
   
   
 end
